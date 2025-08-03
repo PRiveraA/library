79e85c24703749cd1d54430809101ebcbbfe65e0
Pedro Rivera <priveraa94@gmail.com>
2025-08-03 03:37:20 -0400

    Adds book card elements to the dom, Minor css adjustments

--- main.js
+++ main.js
@@ -19,7 +19,45 @@ addBook("The Lies of Lock Lamora", "Scott Lynch", 499, "no")
 addBook("The Martian", "Andy Weir", 369, "no")
 addBook("The Black Company", "Glen Cook", 319, "no")
 
+// Book Creation
+const cardContainer = document.querySelector(".card-container")
 for (const book of myLibrary) {
-  console.log(book.author)
-  // Dynamic html goes here
+  console.log(book)
+  // Card
+  const card = document.createElement("div")
+  card.classList.add("card")
+  cardContainer.appendChild(card)
+  // Details
+  const details = document.createElement("div")
+  details.classList.add("details")
+  card.appendChild(details)
+  // Title
+  const title = document.createElement("h2")
+  title.classList.add("title")
+  title.textContent = book.title
+  details.appendChild(title)
+  // Author
+  const author = document.createElement("p")
+  author.classList.add("author")
+  author.textContent = book.author
+  details.appendChild(author)
+  // Pages
+  const pages = document.createElement("p")
+  pages.classList.add("pages")
+  pages.textContent = "Pages: " + book.pages
+  details.appendChild(pages)
+  // Read
+  const read = document.createElement("div")
+  read.classList.add("read")
+  details.appendChild(read)
+  // Read text
+  const text = document.createElement("p")
+  text.classList.add("text")
+  text.textContent = "Read: "
+  read.appendChild(text)
+  // Read stat
+  const stat = document.createElement("p")
+  stat.classList.add("stat")
+  stat.textContent = book.read
+  read.appendChild(stat)
 }
